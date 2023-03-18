e710db52d188c66fb477221e07f72084cc32636e
Carlos E Alford <carlos.alford@gmail.com>
2023-03-18 13:56:50 +0000

    Backend stores and retrieves chat data

--- server/services/analytics.service.js
+++ server/services/analytics.service.js
@@ -1,15 +1,21 @@
 const { knex } = require('../database');
 
 async function getPage({ page = 1, pageSize = 10 } = {}) {
+    const records = await knex.select("").from("analytics");
     return {
-        count: 0,
-        items: []
+        count: records.length,
+        items: records
     };
 }
 
 async function record(message, intent, replies) {
-
-}
+    // keep track of existing all intents, even null, for analytics
+    await knex("analytics").insert({
+        message: message,
+        intent: intent?.name || intent,
+        replies: replies
+    });
+};
 
 module.exports = {
     getPage,
