a63a432ffbdc9f7d9f5c17dce172e7d593312686
Carlos E Alford <carlos.alford@gmail.com>
2023-03-18 15:10:58 +0000

    Chat page logic take user query and displays bot response
    Uses state to keep track of the conversation and display it for the user to see

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { FormControl, TextField, Button } from '@mui/material';
+
 
 const Chat = () => {
+    // store each question, response as items
+    const [currentChat, setCurrentChat] = useState([]);
+    
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const userQuery = e.target[0].value;
+
+        const botReply = await fetch('http://localhost:5000/api/chat', {
+            method: 'POST',
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "message": userQuery
+            })
+        }).then(res => {
+            if (res.status === 200) {
+                return res.json();
+            }
+            return null;
+        });
+
+        // TODO: how to show a network error to the user
+        if (botReply !== null) {
+            setCurrentChat(convo => [...convo, userQuery, botReply[0].text]);
+        }
+        // prepare input for next query
+        e.target[0].value = '';
+    }
+
+
     // TODO build chat page
     return (
-        <>
-        </>
+        <section>
+            <ul>
+                {currentChat.map((line, i) => <li key={i}>{line}</li>)}
+            </ul>
+            <form onSubmit={handleSubmit}>
+                <FormControl>
+                    <TextField id='user-query-multiline' label="enter query" required />
+                    <Button type="submit">Submit</Button>
+                </FormControl>
+            </form>
+        </section>
     )
 }
 
